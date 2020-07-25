f0faac5012a17520c31084c14f958220df02fa1d
rach-roi <rach-roi@users.noreply.github.com>
2020-07-25 12:28:16 +0200

    Added all html, css and JS code up to 25/07/2020

---
+++ src/index.js
@@ -0,0 +1,104 @@
+//Feature 1 - current day and time (real)
+
+function formatDate(dateTime) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dateTime.getDay()];
+  let hours = dateTime.getHours();
+  let minutes = dateTime.getMinutes();
+
+  let currentDay = document.querySelector("#current-day");
+  currentDay.innerHTML = day.toUpperCase();
+
+  let currentTime = document.querySelector("#current-time");
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  currentTime.innerHTML = `${hours}:${minutes}`;
+}
+
+let currentDateTime = new Date();
+formatDate(currentDateTime);
+
+//Feature 2 - change city heading based on search input, and display temperature
+
+function displayTemp(response) {
+  let temp = response.data.main.temp;
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = `${temp}`;
+
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+
+  if (response.data.name === "Arrondissement de Lyon") {
+    cityName.innerHTML = "Lyon, FR";
+  }
+}
+
+function changeCityName(event) {
+  event.preventDefault();
+  let searchCityInput = document.querySelector("#search-text-input");
+
+  let city = searchCityInput.value;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d3aa3f4910ac41a0bf73215274652b40`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+let changeCityForm = document.querySelector("#change-city-form");
+changeCityForm.addEventListener("submit", changeCityName);
+
+//get temperature in current location and display
+
+function displayTempCurrentLocation(response) {
+  let currentLocation = response.data.name;
+  let country = response.data.sys.country;
+  let temp = response.data.main.temp;
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = `${temp}`;
+
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = `${currentLocation}, ${country}`;
+}
+
+function getPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=d3aa3f4910ac41a0bf73215274652b40`;
+
+  axios.get(apiUrl).then(displayTempCurrentLocation);
+}
+
+function getTempCurrentLocation(event) {
+  navigator.geolocation.getCurrentPosition(getPosition);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getTempCurrentLocation);
+
+/*Feature 3 - change C degrees to F (fake data) <-- will eventually convert and swap C and F
+
+function degreesFahr() {
+  let currentDegrees = document.querySelector("#current-temperature");
+  currentDegrees.innerHTML = `77`;
+}
+
+function degreesCelsius() {
+  let currentDegrees = document.querySelector("#current-temperature");
+  currentDegrees.innerHTML = `25`;
+}
+
+let fahrSymbol = document.querySelector("#fahrenheit");
+fahrSymbol.addEventListener("click", degreesFahr);
+
+let celsiusSymbol = document.querySelector("#celsius");
+celsiusSymbol.addEventListener("click", degreesCelsius);
+*/
